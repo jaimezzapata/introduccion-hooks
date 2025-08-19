dbc1d3e60354133fa3fedf4fb75f8ca5ae2e226b
jaimezzapata <zapataval2304@gmail.com>
2025-08-19 08:55:30 -0500

    feature: creación de los componetes y el enrutamiento básico sin funcionalidad

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,10 @@
+/* rfce -> crea un componente con función regular */
+/* rafce -> crea un componente con función flecha */
+
+function Contacto() {
+  return (
+    <div>Contacto</div>
+  )
+}
+
+export default Contacto
